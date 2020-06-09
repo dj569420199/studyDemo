dcdcb744d255455e29ff93337214c5709b2d5452
flyingfish <dj569420199@126.com>
2020-06-09 18:43:43 +0800

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# studyDemo
-study demo
---
+++ src/test/java/com/study/demo/AppTest.java
@@ -0,0 +1,20 @@
+package com.study.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
