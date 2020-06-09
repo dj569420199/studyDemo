ec1767ce8f36f7118640a3fe34ccf3918f2ea776
flyingfish <dj569420199@126.com>
2020-06-09 19:40:29 +0800

    test app

--- src/main/java/com/study/demo/App.java
+++ src/main/java/com/study/demo/App.java
@@ -2,12 +2,13 @@ package com.study.demo;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        ThreadLocal threadLocal = new ThreadLocal();
+        threadLocal.set("dddd");
+        System.out.println(threadLocal.get());
+        System.out.println("Hello World!");
     }
 }
